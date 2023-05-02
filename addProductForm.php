b9818f3988bbb805253ed1f1410420cb36ac646f
khanfahim <samarkhan616264@gmail.com>
2023-05-02 11:13:55 +0530

    label change

--- addProductForm.php
+++ addProductForm.php
@@ -4,60 +4,69 @@
     <form class="machineAddForm" action="product.php" method="post" enctype="multipart/form-data">
         <div class="row">
             <div class="col-25">
-        <label for="productName">Product Name</label>
-        </div>
-        <div class="col-25">
-        <input type="text" name="productName" id="productName" required>
-        </div>
-        <div class="col-25">
-        <label for="productModal">Modal No.</label>
-        </div>
-        <div class="col-25">
-        <input type="text" name="productModal" id="productModal" required>
-        </div>
+                <label for="productName">Product Name</label>
+            </div>
+            <div class="col-25">
+                <input type="text" name="productName" id="productName" required>
+            </div>
+            <div class="col-25">
+                <label for="productModal">Modal No.</label>
+            </div>
+            <div class="col-25">
+                <input type="text" name="productModal" id="productModal" required>
+            </div>
         </div>
         <div class="row">
-        <div class="col-25">
-        <label for="productImg">Image</label>
+            <div class="col-25">
+                <label for="productImg">Image</label>
+            </div>
+            <div class="col-25">
+                <input type="file" name="productImg" id="productImg" accept="image/png,image/jpg,image/jpeg" required>
+            </div>
+            <div class="col-25">
+                <label for="productQuantity">Quantity</label>
+            </div>
+            <div class="col-25">
+                <input type="number" name="productQuantity" id="productQuantity" required>
+            </div>
         </div>
-        <div class="col-25">
-        <input type="file" name="productImg" id="productImg" accept="image/png,image/jpg,image/jpeg" required>
+        <div class="row4">
+            <div class="col-25">
+                <label for="productNormalPrice">Normal Price</label>
+            </div>
+            <div class="col-25">
+                <input type="number" name="productNormalPrice" id="productNormalPrice" required>
+            </div>
+            <div class="col-25">
+                <label for="productDiscountedPrice">Discounted Price</label>
+            </div>
+            <div class="col-25">
+                <input type="number" name="productDiscountedPrice" id="productDiscountedPrice" required>
+            </div>
         </div>
-        <div class="col-25">
-        <label for="productQuantity">Quantity</label>
+        <div class="row4">
+        <div class="col-25 col-26">
+                <label for="productCategory">Select Category</label>
         </div>
-        <div class="col-25">
-        <input type="number" name="productQuantity" id="productQuantity" required>
-<<<<<<< HEAD
+           
+            <div class="col-25 col-26">
+                <select name="productCategory" id="productCategory" required>
+                    <option value="" selected disabled>Select Category</option>
+                    <?php
+                    $pCatSql = "select * from pro_category";
+                    $pCatResult = mysqli_query($conn, $pCatSql);
+                    if (mysqli_num_rows($pCatResult) > 0) {
+                        while ($pCatRow = mysqli_fetch_assoc($pCatResult)) {
+                            ?>
+                            <option value="<?php echo $pCatRow['P_Category_Id'] ?>"><?php echo $pCatRow['P_Category_Name'] ?>
+                            </option>
+                            <?php
+                        }
+                    }
+                    ?>
+                </select>
+            </div>
         </div>
-</div>
-<div class="row4">
-<div class="col-25 col-26">
-=======
-        <label for="productNormalPrice">Normal Price</label>
-        <input type="number" name="productNormalPrice" id="productNormalPrice" required>
-        <label for="productDiscountedPrice">Discounted Price</label>
-        <input type="number" name="productDiscountedPrice" id="productDiscountedPrice" required>
->>>>>>> daa4fc46463060eaa697e7f313e5d1944030a9ea
-        <label for="productCategory">Select Category</label>
-</div>
-<div class="col-25 col-26">
-        <select name="productCategory" id="productCategory" required>
-            <option value="" selected disabled>Select Category</option>
-        <?php
-        $pCatSql="select * from pro_category";
-        $pCatResult=mysqli_query($conn,$pCatSql);
-        if(mysqli_num_rows($pCatResult)>0){
-            while($pCatRow=mysqli_fetch_assoc($pCatResult)){
-        ?>
-            <option value="<?php echo $pCatRow['P_Category_Id'] ?>"><?php echo $pCatRow['P_Category_Name'] ?></option>
-        <?php
-            }
-        }
-        ?>
-        </select>
-</div>
-</div>
         <div class="addMachineFormBtn">
             <input class="btn" type="submit" value="Add">
             <a class="btn" href="product.php">Cancel</a>
