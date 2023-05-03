9b0696f529d3de22b10949c37a042ccb60cbe705
Pramod <pramod52jpr@gmail.com>
2023-05-03 04:42:21 -0700

    change

--- orders.php
+++ orders.php
@@ -350,7 +350,7 @@ if(isset($_POST['odrId']) and isset($_POST['approvedPrice'])){
                             ?>
                             <input type="hidden" name="oderStatus" value="<?php echo $aorow['Order_Status'] ?>">
                             <input type="hidden" name="oderId" value="<?php echo $aorow['Order_Id'] ?>">
-                            <label for="deliveryDate">Delivery Date</label>
+                            <label for="deliveryDate" style="color:green">Delivery Date</label>
                             <input class="order-input" type="date" max="<?php echo date("Y-m-d") ?>" name="deliveryDate" id="deliveryDate" value="<?php echo $aorow['Delivery_Date'] ?>" <?php echo $disabling ?>>
                             <input class="order-btn" type="submit" value="save" <?php echo $disabling ?>>
                         </form>
