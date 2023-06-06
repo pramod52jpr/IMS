e7fff51c459a779f1f9d0e57d25c4bd56756278c
Pramod <pramod52jpr@gmail.com>
2023-06-05 22:19:03 -0700

    small change

--- dbBackup.php
+++ dbBackup.php
@@ -1,7 +1,8 @@
 <?php
+include "conn.php";
 include_once('./dbbackup/Mysqldump/Mysqldump.php');
-$dump = new Ifsnop\Mysqldump\Mysqldump('mysql:host=localhost;dbname=ims', 'root', '');
-$date=date("d-m-Y");
+$dump = new Ifsnop\Mysqldump\Mysqldump("mysql:host=$hostname;dbname=$db_name", $username, $db_password);
+$date=date("d-m-Y-i");
 $dump->start('./dbbackup/database/'.$date.'.sql');
 
 ?>
